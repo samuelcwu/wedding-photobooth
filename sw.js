cf5bea7fd601648d116f18af98fad1c1a05e0ba5
Samuel Wu <samuel.wu96@gmail.com>
2026-06-23 00:20:14 -0400

    Fix sharing: open image in Safari so full share sheet shows Canon Mini Print

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'photobooth-v6';
+const CACHE = 'photobooth-v7';
 const FILES = ['./index.html', './manifest.json', './sw.js', './icon.svg'];
 
 self.addEventListener('install', event => {
