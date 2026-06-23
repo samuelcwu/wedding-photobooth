0e599400ea9afcb7f2245b9cfe84e033b748d873
Samuel Wu <samuel.wu96@gmail.com>
2026-06-22 23:38:42 -0400

    Add debug overlay, harden camera init with fallback constraints

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'photobooth-v2';
+const CACHE = 'photobooth-v3';
 const FILES = ['./index.html', './manifest.json', './sw.js', './icon.svg'];
 
 self.addEventListener('install', event => {
